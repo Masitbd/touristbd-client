dad1d4560aad333c242c5d51d0521dd2fdc76a29
Murad Sarker <sarker24@gmail.com>
2021-10-30 13:00:47 +0200

    Add the header

--- src/Pages/Header/NavBar.js
+++ src/Pages/Header/NavBar.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <div>
-      <h2>thsi is nabbar page</h2>
+    <div className="header">
+      <nav>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active" to="/services">
+          Services
+        </NavLink>
+        <NavLink activeClassName="active" to="/departments">
+          Departments
+        </NavLink>
+        <NavLink activeClassName="active" to="/gallery">
+          Gallery
+        </NavLink>
+        <NavLink activeClassName="active" to="/about">
+          About
+        </NavLink>
+      </nav>
     </div>
   );
 };
 
 export default NavBar;
-<h2>thsi is nabbar page</h2>;
